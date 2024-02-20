9e857722255c6a1b02fb165b739ccbc28a8180f8
hiimsewek <102280@stud.uz.zgora.pl>
2024-02-20 18:35:31 +0100

    Add date utility functions

---
+++ src/utils/date.ts
@@ -0,0 +1,22 @@
+export const separateDate = (date: Date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return { year, month, day };
+};
+
+export const getCurrentYear = () => {
+  const currentYear = new Date().getFullYear().toString();
+  return new Date(currentYear);
+};
+
+export const getTodaysDate = () => {
+  return new Date();
+};
+
+export const getFormattedDate = (date: Date) => {
+  const { day, month, year } = separateDate(date);
+
+  return `${month}/${day}/${year}`;
+};
