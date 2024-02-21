cbd8ae20d1e5aa857261501a9bfb074b1c8fb7f0
hiimsewek <102280@stud.uz.zgora.pl>
2024-02-21 10:17:48 +0100

    Implement delete and edit expense functions

--- src/stores/expenses.ts
+++ src/stores/expenses.ts
@@ -1,18 +1,38 @@
-import { reactive } from "vue";
+import { computed, reactive } from "vue";
 import { defineStore } from "pinia";
 import type { Expense } from "@/types";
 import { getStoredData, storeData } from "@/utils/storage";
 
-const initialExpenses = getStoredData<Expense[]>("expenses", []);
+const STORE_KEY = "expenses";
 
-export const useExpenses = defineStore("expenses", () => {
-  const expenses = reactive(initialExpenses);
+export const useExpenses = defineStore(STORE_KEY, () => {
+  const initialState = getStoredData<Expense[]>(STORE_KEY, []);
+
+  const expenses = reactive(initialState);
+  const total = computed(() => {
+    return expenses.reduce((result, expense) => (result += expense.amount), 0);
+  });
 
   const addExpense = (item: Expense) => {
     expenses.push(item);
-
-    const storedExpenses = getStoredData<Expense[]>("expenses", []);
-    storeData("expenses", [...storedExpenses, item]);
+    storeData(STORE_KEY, expenses);
   };
-  return { expenses, addExpense };
+
+  function deleteExpense(id: string) {
+    const itemIdx = expenses.findIndex((el: Expense) => el.id === id);
+
+    if (itemIdx === -1) return;
+    expenses.splice(itemIdx, 1);
+    storeData(STORE_KEY, expenses);
+  }
+
+  const editExpense = (id: string, data: Expense) => {
+    const itemIdx = expenses.findIndex((el: Expense) => el.id === id);
+
+    if (itemIdx === -1) return;
+    expenses.splice(itemIdx, 1, data);
+    storeData<Expense[]>(STORE_KEY, expenses);
+  };
+
+  return { expenses, addExpense, total, deleteExpense, editExpense };
 });
