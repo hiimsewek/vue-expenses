0eeeddc53ce378552ec10c2991fd9e06660d712a
hiimsewek <102280@stud.uz.zgora.pl>
2024-02-21 12:03:49 +0100

    Add edit expense form

---
+++ src/components/EditExpenseForm.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+import Calendar from "primevue/calendar";
+import FloatLabel from "primevue/floatlabel";
+import InputNumber from "primevue/inputnumber";
+import InputText from "primevue/inputtext";
+import { computed, ref, type Ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
+import { storeToRefs } from "pinia";
+import { getCurrentYear, getTodaysDate } from "@/utils/date";
+import { useExpenses } from "@/stores/expenses";
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  visible: {
+    type: Boolean,
+    required: true,
+  },
+  onClose: {
+    type: Function,
+    required: true,
+  },
+});
+
+const expensesStore = useExpenses();
+const { expenses } = storeToRefs(expensesStore);
+
+const itemToEdit = expenses.value.find((expense) => expense.id === props.id);
+
+const date: Ref<string | null> = ref(itemToEdit?.date || null);
+const category = ref(itemToEdit?.category);
+const amount: Ref<number | null> = ref(itemToEdit?.amount || null);
+
+const submitDisabled = computed(() => {
+  const dateDisabled = !date.value || date.value === itemToEdit?.date;
+
+  const categoryDisabled =
+    !category.value || category.value === itemToEdit?.category;
+  const amountDisabled = !amount.value || amount.value === itemToEdit?.amount;
+
+  return dateDisabled && categoryDisabled && amountDisabled;
+});
+
+const submitEditHandler = () => {
+  if (!date.value || !category.value || !amount.value) return;
+
+  const id = uuidv4() as string;
+
+  const data = {
+    id,
+    date: date.value,
+    category: category.value,
+    amount: amount.value,
+  };
+
+  expensesStore.editExpense(props.id, data);
+
+  props.onClose();
+};
+</script>
+
+<template>
+  <Dialog
+    :visible="props.visible"
+    @update:visible="props.onClose"
+    modal
+    header="Edit Expense"
+    :draggable="false"
+    dismissableMask
+  >
+    <div class="editExpenseFormContainer">
+      <FloatLabel class="itemSpacing">
+        <Calendar
+          v-model="date"
+          inputId="newExpenseDate"
+          selectOtherMonths
+          placeholder="mm/dd/yyyy"
+          :min-date="getCurrentYear()"
+          :max-date="getTodaysDate()"
+        />
+        <label for="newExpenseDate">Date</label>
+      </FloatLabel>
+
+      <FloatLabel class="itemSpacing">
+        <InputText v-model="category" inputId="newExpenseCategory" />
+        <label for="newExpenseCategory">Category</label>
+      </FloatLabel>
+
+      <FloatLabel class="itemSpacing">
+        <InputNumber
+          v-model="amount"
+          inputId="newExpenseAmount"
+          :max-fraction-digits="2"
+        />
+        <label for="newExpenseAmount">Amount</label>
+      </FloatLabel>
+      <Button
+        label="Edit Expense"
+        class="itemSpacing"
+        :disabled="submitDisabled"
+        @click="submitEditHandler()"
+      />
+    </div>
+  </Dialog>
+</template>
+
+<style scoped>
+.itemSpacing {
+  margin: 20px 0;
+}
+
+.itemSpacing:last-of-type {
+  margin: 20px 0 0 0;
+}
+
+.editExpenseFormContainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/components/ExpensesTable.vue
+++ src/components/ExpensesTable.vue
@@ -2,32 +2,56 @@
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import { storeToRefs } from "pinia";
+import { ref, type Ref } from "vue";
 import { useExpenses } from "@/stores/expenses";
+import EditExpenseForm from "./EditExpenseForm.vue";
 
 const expensesStore = useExpenses();
 const { expenses, total } = storeToRefs(expensesStore);
+
+const itemToEdit: Ref<string | null> = ref(null);
+
+const setItemToEdit = (id: string) => {
+  itemToEdit.value = id;
+};
+
+const closeEditHandler = () => {
+  itemToEdit.value = null;
+};
 </script>
 
 <template>
-  <div v-if="expenses.length > 0" class="expenseWrapper">
-    <DataTable :value="expenses" scrollable stripedRows removableSort>
-      <Column header="Date" field="date" sortable></Column>
-      <Column header="Category" field="category" sortable></Column>
-      <Column header="Amount" field="amount" sortable></Column>
-      <Column header="Actions">
-        <template #body="{ data }">
-          <i class="pi pi-pencil" aria-label="Edit" />
-          <i
-            class="pi pi-trash"
-            aria-label="Delete"
-            @click="expensesStore.deleteExpense(data.id)"
-          />
-        </template>
-      </Column>
-      <template #footer>Total: {{ total }}</template>
-    </DataTable>
+  <div>
+    <div v-if="expenses.length > 0" class="expenseWrapper">
+      <DataTable :value="expenses" scrollable stripedRows removableSort>
+        <Column header="Date" field="date" sortable></Column>
+        <Column header="Category" field="category" sortable></Column>
+        <Column header="Amount" field="amount" sortable></Column>
+        <Column header="Actions">
+          <template #body="{ data }">
+            <i
+              class="pi pi-pencil"
+              aria-label="Edit"
+              @click="setItemToEdit(data.id)"
+            />
+            <i
+              class="pi pi-trash"
+              aria-label="Delete"
+              @click="expensesStore.deleteExpense(data.id)"
+            />
+          </template>
+        </Column>
+        <template #footer>Total: {{ total }}</template>
+      </DataTable>
+    </div>
+    <span v-else>You don't have any expenses yet.</span>
   </div>
-  <span v-else>You don't have any expenses yet.</span>
+  <EditExpenseForm
+    v-if="itemToEdit"
+    :id="itemToEdit"
+    :visible="!!itemToEdit"
+    :on-close="closeEditHandler"
+  />
 </template>
 
 <style scoped>
