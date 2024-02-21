144e71df616a1234dea3518707c98a8df0875718
hiimsewek <102280@stud.uz.zgora.pl>
2024-02-21 21:48:51 +0100

    Add getMonthAndYear utility function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -20,3 +20,10 @@ export const getFormattedDate = (date: Date) => {
 
   return `${month}/${day}/${year}`;
 };
+
+export const getMonthAndYear = (date: Date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+
+  return `${month}/${year}`;
+};
