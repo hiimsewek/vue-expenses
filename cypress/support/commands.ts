7fda72b0d28a7233c08c74f37026517c0f2fd3f2
hiimsewek <102280@stud.uz.zgora.pl>
2024-02-27 21:58:50 +0100

    Add getData and addExpense commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,39 +1,27 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
 
-export {}
+Cypress.Commands.add("getData", (attribute) => {
+  return cy.get(`[data-test="${attribute}"]`);
+});
+
+Cypress.Commands.add("addExpense", (date, category, amount) => {
+  cy.getData("newExpenseDate").type(`${date}{enter}`);
+  cy.getData("newExpenseCategory").type(category);
+  cy.getData("newExpenseAmount").type(amount.toString());
+  cy.getData("addExpenseBtn").click();
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      getData(attribute: string): Chainable<JQuery<HTMLElement>>;
+      addExpense(
+        date: string,
+        category: string,
+        amount: number
+      ): Chainable<void>;
+    }
+  }
+}
+
+export {};
